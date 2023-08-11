571160a0b27c8a85c39284d552ec09a165102dbb
Eman Azzam <besimin89@gmail.com>
2023-08-11 21:31:06 +0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-    req.send({ hi: 'Besimin' });
+    res.send({ hi: 'Besimin' });
 });
 
 const PORT = process.env.PORT || 5001;
